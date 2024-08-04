76f1ce7ed0ed75f48af9ac62a88f6d9d89760628
Prasenjit Das <prasanjitacademic@gmail.com>
2024-08-04 09:52:15 +0530

    fluids done with network image

--- lib/screens/eleven_screens/fluids.dart
+++ lib/screens/eleven_screens/fluids.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:formula_app/models/scroll_to_bottom.dart';
 import 'package:formula_app/widgets/custom_app_bar_title.dart';
-import 'package:formula_app/widgets/double_tap_to_zoom_widget.dart';
 import 'package:formula_app/widgets/gap.dart';
 import 'package:formula_app/widgets/double_tap_to_zoom_network_widget.dart';
 import 'package:formula_app/widgets/my_divider.dart';
@@ -77,136 +76,189 @@ class _FluidsState extends State<Fluids> {
               textAlign: TextAlign.center,
             ),
             Gap(10),
-            DoubleTapToZoomWidget(imageHeight: 250, imageName: "fluid1.jpg"),
+            DoubleTapToZoomNetworkWidget(
+                imageHeight: 250, imageName: "fluid1.jpg"),
             Gap(10),
             MyDivider(
                 dividerText: "MIXING OF LIQUID\nCalculation of final Density"),
-            DoubleTapToZoomWidget(imageHeight: 150, imageName: "fluid2.jpg"),
+            DoubleTapToZoomNetworkWidget(
+                imageHeight: 150, imageName: "fluid2.jpg"),
             Gap(10),
-            DoubleTapToZoomWidget(imageHeight: 230, imageName: "fluid3.jpg"),
+            DoubleTapToZoomNetworkWidget(
+                imageHeight: 230, imageName: "fluid3.jpg"),
             Gap(10),
-            DoubleTapToZoomWidget(imageHeight: 200, imageName: "fluid4.jpg"),
+            DoubleTapToZoomNetworkWidget(
+                imageHeight: 200, imageName: "fluid4.jpg"),
             Gap(10),
             MyDivider(dividerText: "Relative Density (R.D)/Specific Gravity"),
-            DoubleTapToZoomWidget(imageHeight: 350, imageName: "fluid5.jpg"),
+            DoubleTapToZoomNetworkWidget(
+                imageHeight: 350, imageName: "fluid5.jpg"),
             Gap(10),
-            DoubleTapToZoomWidget(imageHeight: 200, imageName: "fluid6.jpg"),
+            DoubleTapToZoomNetworkWidget(
+                imageHeight: 200, imageName: "fluid6.jpg"),
             Gap(10),
-            DoubleTapToZoomWidget(imageHeight: 160, imageName: "fluid7.jpg"),
+            DoubleTapToZoomNetworkWidget(
+                imageHeight: 160, imageName: "fluid7.jpg"),
             Gap(10),
             MyDivider(dividerText: "PRESSURE"),
-            DoubleTapToZoomWidget(imageHeight: 250, imageName: "fluid8.jpg"),
+            DoubleTapToZoomNetworkWidget(
+                imageHeight: 250, imageName: "fluid8.jpg"),
             Gap(10),
-            DoubleTapToZoomWidget(imageHeight: 300, imageName: "fluid9.jpg"),
+            DoubleTapToZoomNetworkWidget(
+                imageHeight: 300, imageName: "fluid9.jpg"),
             Gap(10),
             MyDivider(dividerText: "INCLINED BAROMETER"),
-            DoubleTapToZoomWidget(imageHeight: 400, imageName: "fluid10.jpg"),
+            DoubleTapToZoomNetworkWidget(
+                imageHeight: 400, imageName: "fluid10.jpg"),
             Gap(10),
-            DoubleTapToZoomWidget(imageHeight: 400, imageName: "fluid11.jpg"),
+            DoubleTapToZoomNetworkWidget(
+                imageHeight: 400, imageName: "fluid11.jpg"),
             Gap(10),
             MyDivider(dividerText: "BUBBLE RISING UP\nAT CONSTANT TEMPERATURE"),
-            DoubleTapToZoomWidget(imageHeight: 350, imageName: "fluid12.jpg"),
+            DoubleTapToZoomNetworkWidget(
+                imageHeight: 350, imageName: "fluid12.jpg"),
             Gap(10),
-            DoubleTapToZoomWidget(imageHeight: 450, imageName: "fluid13.jpg"),
+            DoubleTapToZoomNetworkWidget(
+                imageHeight: 450, imageName: "fluid13.jpg"),
             Gap(10),
-            DoubleTapToZoomWidget(imageHeight: 400, imageName: "fluid14.jpg"),
+            DoubleTapToZoomNetworkWidget(
+                imageHeight: 400, imageName: "fluid14.jpg"),
             Gap(10),
-            DoubleTapToZoomWidget(imageHeight: 700, imageName: "fluid15.jpg"),
+            DoubleTapToZoomNetworkWidget(
+                imageHeight: 700, imageName: "fluid15.jpg"),
             Gap(10),
-            DoubleTapToZoomWidget(imageHeight: 450, imageName: "fluid16.jpg"),
+            DoubleTapToZoomNetworkWidget(
+                imageHeight: 450, imageName: "fluid16.jpg"),
             Gap(10),
-            DoubleTapToZoomWidget(imageHeight: 450, imageName: "fluid17.jpg"),
+            DoubleTapToZoomNetworkWidget(
+                imageHeight: 450, imageName: "fluid17.jpg"),
             Gap(10),
-            DoubleTapToZoomWidget(imageHeight: 550, imageName: "fluid18.jpg"),
+            DoubleTapToZoomNetworkWidget(
+                imageHeight: 550, imageName: "fluid18.jpg"),
             Gap(10),
-            DoubleTapToZoomWidget(imageHeight: 450, imageName: "fluid19.jpg"),
+            DoubleTapToZoomNetworkWidget(
+                imageHeight: 450, imageName: "fluid19.jpg"),
             Gap(10),
             MyDivider(dividerText: "U-Tube Accelerating Horizontally"),
-            DoubleTapToZoomWidget(imageHeight: 650, imageName: "fluid20.jpg"),
+            DoubleTapToZoomNetworkWidget(
+                imageHeight: 650, imageName: "fluid20.jpg"),
             Gap(10),
             MyDivider(dividerText: "Special Case: U-Tube Rotating"),
-            DoubleTapToZoomWidget(imageHeight: 500, imageName: "fluid21.jpg"),
+            DoubleTapToZoomNetworkWidget(
+                imageHeight: 500, imageName: "fluid21.jpg"),
             Gap(10),
-            DoubleTapToZoomWidget(imageHeight: 450, imageName: "fluid22.jpg"),
+            DoubleTapToZoomNetworkWidget(
+                imageHeight: 450, imageName: "fluid22.jpg"),
             Gap(10),
-            DoubleTapToZoomWidget(imageHeight: 250, imageName: "fluid23.jpg"),
+            DoubleTapToZoomNetworkWidget(
+                imageHeight: 250, imageName: "fluid23.jpg"),
             Gap(10),
             MyDivider(dividerText: "APPLICATION OF PASCAL'S LAW"),
-            DoubleTapToZoomWidget(imageHeight: 350, imageName: "fluid24.jpg"),
+            DoubleTapToZoomNetworkWidget(
+                imageHeight: 350, imageName: "fluid24.jpg"),
             Gap(10),
-            DoubleTapToZoomWidget(imageHeight: 350, imageName: "fluid25.jpg"),
+            DoubleTapToZoomNetworkWidget(
+                imageHeight: 350, imageName: "fluid25.jpg"),
             Gap(10),
-            DoubleTapToZoomWidget(imageHeight: 400, imageName: "fluid26.jpg"),
+            DoubleTapToZoomNetworkWidget(
+                imageHeight: 400, imageName: "fluid26.jpg"),
             Gap(10),
             MyDivider(dividerText: "LAW OF FLOATATION"),
-            DoubleTapToZoomWidget(imageHeight: 500, imageName: "fluid27.jpg"),
+            DoubleTapToZoomNetworkWidget(
+                imageHeight: 500, imageName: "fluid27.jpg"),
             Gap(10),
-            DoubleTapToZoomWidget(imageHeight: 500, imageName: "fluid28.jpg"),
+            DoubleTapToZoomNetworkWidget(
+                imageHeight: 500, imageName: "fluid28.jpg"),
             Gap(10),
-            DoubleTapToZoomWidget(imageHeight: 450, imageName: "fluid29.jpg"),
+            DoubleTapToZoomNetworkWidget(
+                imageHeight: 450, imageName: "fluid29.jpg"),
             Gap(10),
             MyDivider(dividerText: "FRACTIONAL SUBMERGED VOLUME"),
-            DoubleTapToZoomWidget(imageHeight: 350, imageName: "fluid30.jpg"),
+            DoubleTapToZoomNetworkWidget(
+                imageHeight: 350, imageName: "fluid30.jpg"),
             Gap(10),
-            DoubleTapToZoomWidget(imageHeight: 180, imageName: "fluid31.jpg"),
+            DoubleTapToZoomNetworkWidget(
+                imageHeight: 180, imageName: "fluid31.jpg"),
             Gap(10),
             MyDivider(
                 dividerText:
                     "RELATIVE DENSITY\nOF\nFRACTIONAL SUBMERGED VOLUME"),
-            DoubleTapToZoomWidget(imageHeight: 150, imageName: "fluid32.jpg"),
+            DoubleTapToZoomNetworkWidget(
+                imageHeight: 150, imageName: "fluid32.jpg"),
             Gap(10),
-            DoubleTapToZoomWidget(imageHeight: 150, imageName: "fluid33.jpg"),
+            DoubleTapToZoomNetworkWidget(
+                imageHeight: 150, imageName: "fluid33.jpg"),
             Gap(10),
             MyDivider(dividerText: "NEWTON'S LAW OF VISCOSITY"),
-            DoubleTapToZoomWidget(imageHeight: 300, imageName: "fluid34.jpg"),
+            DoubleTapToZoomNetworkWidget(
+                imageHeight: 300, imageName: "fluid34.jpg"),
             Gap(10),
             MyDivider(dividerText: "UNIT\nOF\nCOEFFICIENT OF VISCOSITY"),
-            DoubleTapToZoomWidget(imageHeight: 200, imageName: "fluid35.jpg"),
+            DoubleTapToZoomNetworkWidget(
+                imageHeight: 200, imageName: "fluid35.jpg"),
             Gap(10),
-            DoubleTapToZoomWidget(imageHeight: 400, imageName: "fluid36.jpg"),
+            DoubleTapToZoomNetworkWidget(
+                imageHeight: 350, imageName: "fluid36.jpg"),
             Gap(10),
-            DoubleTapToZoomWidget(imageHeight: 150, imageName: "fluid37.jpg"),
+            DoubleTapToZoomNetworkWidget(
+                imageHeight: 150, imageName: "fluid37.jpg"),
             Gap(10),
-            DoubleTapToZoomWidget(imageHeight: 380, imageName: "fluid38.jpg"),
+            DoubleTapToZoomNetworkWidget(
+                imageHeight: 380, imageName: "fluid38.jpg"),
             Gap(10),
-            DoubleTapToZoomWidget(imageHeight: 450, imageName: "fluid39.jpg"),
+            DoubleTapToZoomNetworkWidget(
+                imageHeight: 450, imageName: "fluid39.jpg"),
             Gap(10),
             MyDivider(dividerText: "EQUATION OF CONTINUITY"),
-            DoubleTapToZoomWidget(imageHeight: 250, imageName: "fluid40.jpg"),
+            DoubleTapToZoomNetworkWidget(
+                imageHeight: 250, imageName: "fluid40.jpg"),
             Gap(10),
-            DoubleTapToZoomWidget(imageHeight: 250, imageName: "fluid41.jpg"),
+            DoubleTapToZoomNetworkWidget(
+                imageHeight: 250, imageName: "fluid41.jpg"),
             Gap(10),
             MyDivider(dividerText: "ENERGY OF A FLUID\nIN A STEADY FLOW"),
-            DoubleTapToZoomWidget(imageHeight: 280, imageName: "fluid42.jpg"),
+            DoubleTapToZoomNetworkWidget(
+                imageHeight: 280, imageName: "fluid42.jpg"),
             Gap(10),
             MyDivider(dividerText: "BERNOULLI'S PRINCIPLE"),
-            DoubleTapToZoomWidget(imageHeight: 400, imageName: "fluid43.jpg"),
+            DoubleTapToZoomNetworkWidget(
+                imageHeight: 400, imageName: "fluid43.jpg"),
             Gap(10),
             MyDivider(dividerText: "APPLICATIONS OF\nBERNOULLI'S PRINCIPLE"),
-            DoubleTapToZoomWidget(imageHeight: 700, imageName: "fluid44.jpg"),
+            DoubleTapToZoomNetworkWidget(
+                imageHeight: 700, imageName: "fluid44.jpg"),
             Gap(10),
-            DoubleTapToZoomWidget(imageHeight: 400, imageName: "fluid45.jpg"),
+            DoubleTapToZoomNetworkWidget(
+                imageHeight: 400, imageName: "fluid45.jpg"),
             Gap(10),
-            DoubleTapToZoomWidget(imageHeight: 450, imageName: "fluid46.jpg"),
+            DoubleTapToZoomNetworkWidget(
+                imageHeight: 450, imageName: "fluid46.jpg"),
             Gap(10),
-            DoubleTapToZoomWidget(imageHeight: 450, imageName: "fluid47.jpg"),
+            DoubleTapToZoomNetworkWidget(
+                imageHeight: 450, imageName: "fluid47.jpg"),
             Gap(10),
             MyDivider(
                 dividerText:
                     "PRESSURE DIFFERENCE\nACROSS A\nCURVED LIQUID SURFACE"),
-            DoubleTapToZoomWidget(imageHeight: 750, imageName: "fluid48.jpg"),
+            DoubleTapToZoomNetworkWidget(
+                imageHeight: 750, imageName: "fluid48.jpg"),
             Gap(10),
-            DoubleTapToZoomWidget(imageHeight: 450, imageName: "fluid49.jpg"),
+            DoubleTapToZoomNetworkWidget(
+                imageHeight: 450, imageName: "fluid49.jpg"),
             Gap(10),
             MyDivider(dividerText: "SHAPE OF\nLIQUID MENISCUS"),
-            DoubleTapToZoomWidget(imageHeight: 580, imageName: "fluid50.jpg"),
+            DoubleTapToZoomNetworkWidget(
+                imageHeight: 580, imageName: "fluid50.jpg"),
             Gap(10),
             MyDivider(dividerText: "CAPILLARITY"),
-            DoubleTapToZoomWidget(imageHeight: 350, imageName: "fluid51.jpg"),
+            DoubleTapToZoomNetworkWidget(
+                imageHeight: 350, imageName: "fluid51.jpg"),
             Gap(10),
-            DoubleTapToZoomWidget(imageHeight: 350, imageName: "fluid52.jpg"),
+            DoubleTapToZoomNetworkWidget(
+                imageHeight: 350, imageName: "fluid52.jpg"),
             Gap(10),
-            DoubleTapToZoomWidget(imageHeight: 300, imageName: "fluid53.jpg"),
+            DoubleTapToZoomNetworkWidget(
+                imageHeight: 300, imageName: "fluid53.jpg"),
             Gap(10),
           ],
         ),
